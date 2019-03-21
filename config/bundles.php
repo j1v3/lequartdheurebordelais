27b566675499c467524e6a6fe8feb12eaf8863a4
Jeremy VINCENT <jeremy.vincent60@gmail.com>
2019-03-21 17:06:14 +0100

    cookies message

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Shivas\VersioningBundle\ShivasVersioningBundle::class => ['all' => true],
+    Eu\CookieLawBundle\EuCookieLawBundle::class => ['all' => true],
 ];
