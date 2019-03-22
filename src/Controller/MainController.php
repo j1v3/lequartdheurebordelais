b01645f2657d19e3aaca4120d8bc03c0c4982874
Jeremy VINCENT <jeremy.vincent60@gmail.com>
2019-03-22 02:43:34 +0100

    Sidebar basical init

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
   
 use Symfony\Component\Routing\Annotation\Route;
-# use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Contact;
 use App\Form\ContactType;
