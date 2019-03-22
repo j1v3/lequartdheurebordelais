ff2dfde299d07ed1a8bdcae874bdc025e261f361
Jeremy VINCENT <jeremy.vincent60@gmail.com>
2019-03-22 01:01:39 +0100

    Init Contact Page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
   
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-  
-class MainController extends Controller
+# use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+class MainController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
@@ -23,13 +28,6 @@ class MainController extends Controller
     }
   
     /**
-     * @Route("/contact", name="contact")
-     */
-    public function contact() {
-        return $this->render('contact/contact.html.twig');
-    }
-  
-    /**
      * @Route("/search", name="search")
      */
     public function search() {
@@ -43,4 +41,38 @@ class MainController extends Controller
         return $this->render('sidebar/sidebar.html.twig');
     }
  
+     /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request)
+    {
+        $enquiry = new Contact();
+        $form = $this->createForm(ContactType::class, $enquiry);
+     
+         $this->request = $request;
+            if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+     
+     
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from Example')
+                    ->setFrom('contact@lequartdheurebordealis.com')
+                    ->setTo($this->container->getParameter('app.emails.contact_email'))
+                    ->setBody($this->renderView('contact/contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+             
+            $this->get('session')->getFlashbag('blog-notice', 'Your contact enquiry was successfully sent. Thank you!');
+  
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('contact'));
+            }
+        }
+     
+        return $this->render('contact/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
 }
